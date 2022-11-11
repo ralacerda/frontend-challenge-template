caa51446b0f5bc85b79745943b8006b2e2ac3f12
Renato Lacerda <renato.ac.lacerda@gmail.com>
2022-11-11 17:49:46 -0300

    Example counter, html and css

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,13 @@
-<template></template>
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+</script>
+
+<template>
+  <main>
+    <h1>Simple count</h1>
+    <button @click="count++">Increase the count</button>
+    <p>{{ count }}</p>
+  </main>
+</template>
